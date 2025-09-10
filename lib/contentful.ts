e742e866c50121d9847506e8f1df62197d90afd9
gpiazzon <72225531+gpiazzon@users.noreply.github.com>
2025-09-10 15:56:46 +0100

    Add Contentful-powered blog with ISR

---
+++ lib/contentful.ts
@@ -0,0 +1,8 @@
+import { createClient } from "contentful";
+
+export const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID!,
+  environment: process.env.CONTENTFUL_ENVIRONMENT || "master",
+  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN!,
+});
+
